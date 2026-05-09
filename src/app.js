aeb5d93e96adc2938f77f3378e1a752f0ab8838f
shukla-pranjal71 <pran.shukla71@gmail.com>
2026-05-10 01:56:07 +0530

    Register grocery routes in app

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 import express from "express";
 import cors from "cors";
 import userRoutes from "./routes/user.routes.js";
+import groceryRoutes from "./routes/grocery.routes.js";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use("/user", userRoutes);
+app.use("/grocery", groceryRoutes);
 
 export default app;
