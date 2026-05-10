1b342d2a1e826c2e1e10dd35ee7abbb35aab48cf
shukla-pranjal71 <pran.shukla71@gmail.com>
2026-05-10 17:35:01 +0530

    Add route for almost recipes API

--- src/routes/meal.routes.js
+++ src/routes/meal.routes.js
@@ -2,11 +2,13 @@ import express from "express";
 import {
   getMealsHandler,
   markCookedHandler,
+  getAlmostRecipesHandler,
 } from "../controllers/meal.controller.js";
 
 const router = express.Router();
 
 router.get("/", getMealsHandler);
 router.post("/cook", markCookedHandler);
+router.get("/almost", getAlmostRecipesHandler);
 
 export default router;
