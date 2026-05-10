d0b05f8eec672a04d27edc88254421b81af8abeb
shukla-pranjal71 <pran.shukla71@gmail.com>
2026-05-10 21:54:31 +0530

    Add weekly meal planner route

--- src/routes/meal.routes.js
+++ src/routes/meal.routes.js
@@ -3,6 +3,7 @@ import {
   getMealsHandler,
   markCookedHandler,
   getAlmostRecipesHandler,
+  getWeeklyPlanHandler,
 } from "../controllers/meal.controller.js";
 
 const router = express.Router();
@@ -10,5 +11,6 @@ const router = express.Router();
 router.get("/", getMealsHandler);
 router.post("/cook", markCookedHandler);
 router.get("/almost", getAlmostRecipesHandler);
+router.get("/weekly", getWeeklyPlanHandler);
 
 export default router;
